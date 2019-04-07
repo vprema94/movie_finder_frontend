262ec329a8b4918ec1222d48a7d4cf054ed6fbec
Vanessa Prema <vanessabprema@gmail.com>
2019-04-07 13:06:19 -0400

    fixed inline styling

--- src/pages/loadingPage.js
+++ src/pages/loadingPage.js
@@ -9,7 +9,7 @@ class LoadingPage extends Component {
    render() {
       return(
          <div id="loading-background">
-            <h1 style={{color: white}}>Sorry, we are under construction</h1>
+            <h1 style={{color: 'white'}}>Sorry, we are under construction</h1>
             <Image id='loading-pic' style={{width: 50+'px'}} src={loadingPic}/>
          </div>
       )
