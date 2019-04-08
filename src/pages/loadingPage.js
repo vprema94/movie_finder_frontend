25be40559ae3bd5d85e924160eaf73276111fd4e
Vanessa Prema <vanessabprema@gmail.com>
2019-04-08 16:22:14 -0400

    up and running

--- src/pages/loadingPage.js
+++ src/pages/loadingPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Image } from 'semantic-ui-react';
-// import loadingPic from '../images/loading2.gif'
-import loadingPic from '../images/constructionPika.gif'
+import loadingPic from '../images/loading2.gif'
+// import loadingPic from '../images/constructionPika.gif'
 import { withRouter } from 'react-router-dom';
 
 class LoadingPage extends Component {
@@ -9,8 +9,9 @@ class LoadingPage extends Component {
    render() {
       return(
          <div id="loading-background">
-            <h1 style={{color: 'white'}}>Sorry, we are under construction</h1>
-            <Image id='loading-pic' style={{width: 500+'px'}} src={loadingPic}/>
+            {/* <h1 style={{color: 'white'}}>Sorry, we are under construction</h1> */}
+            {/* <Image id='loading-pic' style={{width: 500+'px'}} src={loadingPic}/> */}
+            <Image id='loading-pic' src={loadingPic}/>
          </div>
       )
    }
